4f33d686c9307063083827c1f63a5c0bf1dc813e
Antoni <antonilopezdev@gmail.com>
2026-09-09 07:04:30 +0200

    improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Your Life Story",
   description: "Tu historia, con perspectiva.",
+  verification: {
+    google: "en14B8T4ksTr7A8DRLQktq9UzEYfyOalus4HuynRreI",
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
